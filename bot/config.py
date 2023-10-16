a75a6701598f2bcdc13d8a40f4bff4dcb10e5304
Pejuang Media <106614654+pejuangmedia@users.noreply.github.com>
2023-10-16 20:51:26 +0700

    test

--- bot/config.py
+++ bot/config.py
@@ -19,27 +19,27 @@ from decouple import config
 class Var:
     # Telegram Credentials
 
-    API_ID = config("API_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN", default=None)
+    API_ID = config("API_ID", default="26796802", cast=int)
+    API_HASH = config("API_HASH", default="b8cc96196eb105c33d8ce193e5efff5c")
+    BOT_TOKEN = config("BOT_TOKEN", default="6590624540:AAGOkHMG-k4ykdex2pz3LOOE6CGMZIU-c7s")
 
     # Database Credentials
 
-    REDIS_URI = config("REDIS_URI", default=None)
-    REDIS_PASS = config("REDIS_PASSWORD", default=None)
+    REDIS_URI = config("REDIS_URI", default="redis://default:34957c0584804a6cb1fdc9b4fd9fc4ee@thorough-shrimp-40797.kv.vercel-storage.com:40797")
+    REDIS_PASS = config("REDIS_PASSWORD", default="AZ9dASQgM2VhODk1OTMtOGQzMy00ZWE0LWI4MGUtZmZlOTViN2M4YzdjMzQ5NTdjMDU4NDgwNGE2Y2IxZmRjOWI0ZmQ5ZmM0ZWU")
 
     # Channels Ids
 
-    BACKUP_CHANNEL = config("BACKUP_CHANNEL", default=0, cast=int)
-    MAIN_CHANNEL = config("MAIN_CHANNEL", cast=int)
-    LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
+    BACKUP_CHANNEL = config("BACKUP_CHANNEL", default="-1001946276932", cast=int)
+    MAIN_CHANNEL = config("MAIN_CHANNEL", default="-1001939871276", cast=int)
+    LOG_CHANNEL = config("LOG_CHANNEL", default="-1001292557562", cast=int)
     CLOUD_CHANNEL = config("CLOUD_CHANNEL", cast=int)
-    OWNER = config("OWNER", default=0, cast=int)
+    OWNER = config("OWNER", default="1474271232", cast=int)
 
     # Other Configs
 
     THUMB = config(
-        "THUMBNAIL", default="https://graph.org/file/37d9d0657d51e01a71f26.jpg"
+        "THUMBNAIL", default="https://graph.org/file/04571fee511dd2a0f48b6.jpg"
     )
     FFMPEG = config("FFMPEG", default="ffmpeg")
     SEND_SCHEDULE = config("SEND_SCHEDULE", default=False, cast=bool)
